142497027e339481bae47cb8ec868bdd42cdcb80
Xiaoxing Hu <xiaoxing@iMac.local>
2018-01-20 20:06:25 +1300

    update lexer

--- packages/orga/src/lexer.js
+++ packages/orga/src/lexer.js
@@ -1,6 +1,5 @@
 function Syntax() {
   this.rules = []
-
 }
 
 Syntax.prototype = {
@@ -15,7 +14,7 @@ Syntax.prototype = {
 
 var org = new Syntax()
 
-org.define('headline', /^(\*+)\s+(TODO|DONE)?(?:\s+\[#(A|B|C)\])?(.*?)\s*(:(?:\w+:)+)?$/, (m) => {
+org.define('headline', /^(\*+)\s+(TODO|DONE)?(?:\s+\[#(A|B|C)\])?(.*?)\s*(:(?:\w+:)+)?$/, m => {
   const level = m[1].length
   const keyword = m[2]
   const priority = m[3]
@@ -24,38 +23,38 @@ org.define('headline', /^(\*+)\s+(TODO|DONE)?(?:\s+\[#(A|B|C)\])?(.*?)\s*(:(?:\w
   return { level, keyword, priority, content, tags }
 })
 
-org.define('keyword', /^\s*#\+(\w+):\s*(.*)$/, (m) => {
+org.define('keyword', /^\s*#\+(\w+):\s*(.*)$/, m => {
   const key = m[1]
   const value = m[2]
   return { key, value }
 })
 
 const PLANNING_KEYWORDS = ['DEADLINE', 'SCHEDULED', 'CLOSED']
-org.define('planning', RegExp(`^\\s*(${PLANNING_KEYWORDS.join('|')}):\\s*(.+)$`), (m) => {
+org.define('planning', RegExp(`^\\s*(${PLANNING_KEYWORDS.join('|')}):\\s*(.+)$`), m => {
   const keyword = m[1]
   const timestamp = m[2]
   return { keyword, timestamp }
 })
 
-org.define('block.begin', /^\s*#\+begin_(\w+)(.*)$/i, (m) => {
+org.define('block.begin', /^\s*#\+begin_(\w+)(.*)$/i, m => {
   const type = m[1]
   const params = m[2].split(' ').map( str => str.trim()).filter(String)
   return { type, params }
 })
 
-org.define('block.end', /^\s*#\+end_(\w+)$/i, (m) => {
+org.define('block.end', /^\s*#\+end_(\w+)$/i, m => {
   const type = m[1]
   return { type }
 })
 
 org.define('drawer.end', /^\s*:end:\s*$/i)
 
-org.define('drawer.begin', /^\s*:(\w+):\s*$/, (m) => {
+org.define('drawer.begin', /^\s*:(\w+):\s*$/, m => {
   const type = m[1]
   return { type }
 })
 
-org.define('list.item', /^\s*([-+]|\d+[.)])\s+(.*)$/, (m) => {
+org.define('list.item', /^\s*([-+]|\d+[.)])\s+(.*)$/, m => {
   const bullet = m[1]
   const content = m[2]
   var ordered = true
@@ -68,7 +67,7 @@ org.define('list.item', /^\s*([-+]|\d+[.)])\s+(.*)$/, (m) => {
 
 org.define('table.separator', /^\s*\|-/)
 
-org.define('table.row', /^\s*\|(\s*.+\|)+\s*$/, (m) => {
+org.define('table.row', /^\s*\|(\s*.+\|)+\s*$/, m => {
   const cells = m[1].split('|').map( str => str.trim()).filter(String)
   return { cells }
 })
@@ -77,31 +76,33 @@ org.define('horizontalRule', /^\s*-{5,}\s*$/)
 
 org.define('comment', /^\s*#\s.*$/)
 
-org.define('footnote', /^\[fn:(\w+)\]:\s*(.*)$/, (m) => {
+org.define('footnote', /^\[fn:(\w+)\]:\s*(.*)$/, m => {
   const label = m[1]
   const content = m[2]
   return { label, content }
 })
 
+var inline = new Syntax()
+
 function Lexer() {
 }
 
 Lexer.prototype = {
-  tokenize: function (line) {
+  tokenize: function (input) {
     for ( const { name, pattern, post } of org.rules ) {
-      const m = pattern.exec(line)
+      const m = pattern.exec(input)
       if (!m) { continue }
-      var token = { name }
+      var token = { name, input }
       token.data = post(m)
       return token
     }
 
-    const trimed = line.trim()
+    const trimed = input.trim()
     if (trimed === '') {
-      return { name: `blank` }
+      return { name: `blank`, input }
     }
 
-    return { name: `line`, content: trimed }
+    return { name: `line`, data: { content: trimed } }
   }
 }
 
