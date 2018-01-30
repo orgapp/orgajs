dc7ca99f58a334ad68ee6218681e414661448e76
Xiaoxing Hu <dawnstar.hu@gmail.com>
2018-01-30 14:42:27 +1300

    v0.1.6

---
+++ packages/orga-unified/src/index.js
@@ -0,0 +1,17 @@
+import { Parser as OrgaParser } from 'orga'
+
+function Parser(doc, file) {
+  this.file = file
+  this._parser = new OrgaParser()
+}
+
+Parser.prototype.parse = function() {
+  return this._parser.parse(String(this.file))
+}
+
+module.exports = parse
+parse.Parser = Parser
+
+function parse(options) {
+  this.Parser = Parser
+}
