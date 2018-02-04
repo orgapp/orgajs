ab4cdaebb4243ceb9a5dafe0c2c50ddd38a8aeee
Xiaoxing Hu <xiaoxing@iMac.local>
2018-02-04 15:00:56 +1300

    block

--- packages/oast-to-hast/src/handlers/block.js
+++ packages/oast-to-hast/src/handlers/block.js
@@ -4,16 +4,36 @@ import u from 'unist-builder'
 import { all } from '../transform'
 
 function block(h, node) {
-  const name = node.name
-  if (name.toUpperCase() === `SRC`) {
-    var lang = node.params[0]
-  }
+  const name = node.name.toUpperCase()
+  var tag
+  var pre = true
   var props = {};
-  if (lang) {
-    props.className = ['language-' + lang];
+  switch(name) {
+  case `SRC`:
+    tag = `code`
+    const lang = node.params[0]
+    if (lang) {
+      props.className = ['language-' + lang]
+    }
+    break
+  case `QUOTE`:
+    tag = `blockquote`
+    pre = false
+    break
+  case `COMMENT`:
+    return undefined
+  case `CENTER`:
+    pre = false
+    tag = `center`
+    break
   }
-  var value = node.value || ''
-  return h(node, `pre`, [
-    h(node, 'code', props, [u('text', value)])
-  ])
+
+  var result = u('text', node.value || '')
+  if (tag) {
+    result = h(node, tag, props, [result])
+  }
+  if (pre) {
+    result = h(node, `pre`, [result])
+  }
+  return result
 }
