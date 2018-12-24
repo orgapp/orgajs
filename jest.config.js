e66a55f32ad3eebfb63549ff3d3369474a0c6f85
Xiaoxing Hu <dawnstar.hu@gmail.com>
2018-12-25 12:00:32 +1300

    remove multiline support for inline parsing for performance reason

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  testPathIgnorePatterns: [
+    `/examples/`,
+    `/www/`,
+    `/dist/`,
+    `/node_modules/`,
+    `__tests__/fixtures`,
+  ],
+}
--- packages/orga/src/processors/line.js
+++ packages/orga/src/processors/line.js
@@ -3,19 +3,34 @@ const inlineParse = require('../inline').parse
 
 function process(token, section) {
 
-  var lines = []
+  var nodes = []
   while (this.hasNext()) {
     const token = this.peek()
     // also eats broken block/drawer ends
     if (![`line`, `block.end`, `drawer.end`].includes(token.name)) break
     this.consume()
-    lines.push(token.raw.trim())
+    push(token.raw.trim())
   }
-  const paragraph = new Node(`paragraph`, inlineParse(lines.join(` `)))
-  section.push(paragraph)
+  section.push(new Node(`paragraph`, nodes))
 
   this._aks = {}
   return this.parseSection(section)
+
+  function push(line) {
+    let newNodes = inlineParse(line)
+    // merge text newNodes
+    if (nodes.length > 0 &&
+        nodes[nodes.length - 1].type === `text` &&
+        newNodes.length > 0 &&
+        newNodes[0].type === `text`) {
+      const n = newNodes.shift()
+      let last = nodes.pop()
+      last.value = `${last.value} ${n.value}`
+      nodes.push(last)
+    }
+
+    nodes = [...nodes, ...newNodes]
+  }
 }
 
 module.exports = process
