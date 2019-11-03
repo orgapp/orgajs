8c6ec9ee7794e5a9361a37a04d75cce530014104
Xiaoxing Hu <xiaoxing@huxx.org>
2019-11-03 22:14:21 +1300

    expose Layout and hooks for theme blorg

--- examples/gatsby-blorg/src/pages/404.js
+++ examples/gatsby-blorg/src/pages/404.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Layout from 'gatsby-theme-blorg/src/components/layout'
+import { Layout } from 'gatsby-theme-blorg'
 
 export default () => {
   return (
---
+++ packages/gatsby-theme-blorg/index.js
@@ -0,0 +1,7 @@
+import Layout from './src/components/layout'
+import { useSiteMetadata } from './src/hooks'
+
+export {
+  Layout,
+  useSiteMetadata,
+}
