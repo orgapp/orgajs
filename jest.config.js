6c2dd96dc90f616e71b8d43063484b4d9e7dd2d0
Xiaoxing Hu <xiaoxing@huxx.org>
2020-04-16 19:43:07 +1200

    add missing dependency

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  projects: ['<rootDir>/packages/*/jest.config.js'],
+  // projects: ['<rootDir>/packages/*/jest.config.js'],
   preset: 'ts-jest',
   rootDir: './',
   testPathIgnorePatterns: [
