864f4600ee9f7d011ce4250102c2b6b3a0c86e3e
Xiaoxing Hu <xiaoxing@huxx.org>
2020-07-31 11:32:37 +1200

    fix timezone issue

--- packages/orga/src/tokenize/__tests__/planning.spec.ts
+++ packages/orga/src/tokenize/__tests__/planning.spec.ts
@@ -1,16 +1,20 @@
 import { tokenize } from '../index'
 
+const options = {
+  timezone: 'Pacific/Auckland'
+}
+
 describe('tokenize planning', () => {
 
   it('knows plannings', () => {
-    expect(tokenize('DEADLINE: <2018-01-01 Mon>').all()).toMatchSnapshot()
-    expect(tokenize('  DEADLINE: <2018-01-01 Mon>').all()).toMatchSnapshot()
-    expect(tokenize(' \tDEADLINE: <2018-01-01 Mon>').all()).toMatchSnapshot()
-    expect(tokenize(' \t DEADLINE: <2018-01-01 Mon>').all()).toMatchSnapshot()
+    expect(tokenize('DEADLINE: <2018-01-01 Mon>', options).all()).toMatchSnapshot()
+    expect(tokenize('  DEADLINE: <2018-01-01 Mon>', options).all()).toMatchSnapshot()
+    expect(tokenize(' \tDEADLINE: <2018-01-01 Mon>', options).all()).toMatchSnapshot()
+    expect(tokenize(' \t DEADLINE: <2018-01-01 Mon>', options).all()).toMatchSnapshot()
   })
 
   it('knows these are not plannings', () => {
-    expect(tokenize('dEADLINE: <2018-01-01 Mon>').all()).toMatchSnapshot()
+    expect(tokenize('dEADLINE: <2018-01-01 Mon>', options).all()).toMatchSnapshot()
   })
 
 })
