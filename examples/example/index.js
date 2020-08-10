c50535cb8b4bdaedd4df8e4db77fb3e49e92dd7f
Xiaoxing Hu <xiaoxing@huxx.org>
2020-08-10 17:32:10 +1200

    rename orga-rehype to orga-unified-rehype

--- examples/example/index.js
+++ examples/example/index.js
@@ -1,7 +1,7 @@
 const unified = require('unified')
 const stream = require('unified-stream')
 const parse = require('orga-unified-parse')
-const mutate = require('orga-rehype')
+const mutate = require('orga-unified-rehype')
 const html = require('rehype-stringify')
 
 const processor = unified()
