409f720dd85488990d4f4f201661f48e56f6d7a2
Xiaoxing Hu <xiaoxing@huxx.org>
2020-08-11 18:51:06 +1200

    tweak website

--- www/src/pages/index.js
+++ www/src/pages/index.js
@@ -5,6 +5,7 @@ import { parse } from 'orga'
 import { inspect } from 'util'
 import toHAST from 'oast-to-hast'
 import stringify from 'hast-util-to-html'
+import JSONTree from 'react-json-tree'
 
 const Container = styled.div`
 display: flex;
@@ -19,9 +20,15 @@ width: 100%;
 height: 100%;
 border: none;
 outline: none;
-background-color: #eee8d5;
+background-color: #071404;
+color: #6cf04c;
 padding: 1em;
 border-radius: .5em;
+overflow: auto;
+::selection {
+  color: #d8ffcc;
+  background-color: #43a823;
+}
 `
 
 const Half = styled.div`
@@ -36,7 +43,7 @@ export default () => {
   const [text, setText] = useState('')
   const oast = parse(text)
   const hast = toHAST(oast)
-  const html = stringify(hast)
+  const html = stringify(hast, { allowDangerousHtml: true })
 
   return (
     <Layout>
@@ -53,9 +60,7 @@ export default () => {
       </Container>
       <h1>OAST</h1>
       <p>Org Abstract Syntax Tree</p>
-      <pre>
-        { print(oast) }
-      </pre>
+      <JSONTree data={oast}/>
     </Layout>
   )
 }
