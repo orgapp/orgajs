bac62a4fc47c58143508d3fb061c7c6b0c667bf9
Xiaoxing Hu <xiaoxing@huxx.org>
2020-08-12 20:49:34 +1200

    Updates

---
+++ component---src-pages-page-2-js-c004c6040cb38faeb6b9.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{479:function(e,n,t){"use strict";t.r(n);var a=t(0),o=t.n(a),c=t(92),l=t(474);n.default=function(){return o.a.createElement(l.a,null,o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(c.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-c004c6040cb38faeb6b9.js.map
