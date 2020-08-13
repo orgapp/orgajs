d7aac0958d7ae48405c634371aef5f1d999ecac1
Xiaoxing Hu <xiaoxing@huxx.org>
2020-08-14 11:41:12 +1200

    Updates

---
+++ component---src-pages-404-js-056cb80f3fbcce81b8cc.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("mXGw"),l=n.n(a),u=n("Bl7J");t.default=function(){return l.a.createElement(u.a,null,l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-056cb80f3fbcce81b8cc.js.map
