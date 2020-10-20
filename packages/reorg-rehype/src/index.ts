edd868753836ac54b912d091b0864ec5f8dd8780
Xiaoxing Hu <xiaoxing@huxx.org>
2020-10-20 18:36:48 +1300

    accept partial options

--- packages/reorg-rehype/src/index.ts
+++ packages/reorg-rehype/src/index.ts
@@ -1,7 +1,7 @@
 import { Processor, Transformer } from 'unified'
 import toHAST, { Options } from 'oast-to-hast'
 
-function reorg2rehype (options: Options): Transformer {
+function reorg2rehype (options: Partial<Options>): Transformer {
   return transformer
   function transformer(node) {
     return toHAST(node, options)
