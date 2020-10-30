dc7d260a8247c727365daf08d0e27d3bed9b195b
Xiaoxing Hu <xiaoxing@huxx.org>
2020-10-30 20:00:56 +1300

    fix bug

--- packages/oast-to-hast/src/handlers/section.ts
+++ packages/oast-to-hast/src/handlers/section.ts
@@ -13,12 +13,14 @@ export default (context: Context) => (node: Section): HNode => {
 
   const headline = node.children.find(n => n.type === 'headline') as Headline
   // if (!headline) return undefined
-  if (selectTags.length > 0
-    && !match(selectTags, headline.tags)) {
-    return undefined
-  }
-  if (match(excludeTags, headline.tags)) {
-    return undefined
+  if (headline) {
+    if (selectTags.length > 0
+      && !match(selectTags, headline.tags)) {
+      return undefined
+    }
+    if (match(excludeTags, headline.tags)) {
+      return undefined
+    }
   }
 
   return h('div', { className: ['section'] })(
