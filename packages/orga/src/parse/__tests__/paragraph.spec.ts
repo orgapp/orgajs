12732177601de19e3a50c467049a40f688e8fcee
Ben Moon <software@guiltydolphin.com>
2021-06-29 14:48:41 +0100

    Refactor some tests

--- packages/orga/src/parse/__tests__/paragraph.spec.ts
+++ packages/orga/src/parse/__tests__/paragraph.spec.ts
@@ -51,33 +51,24 @@ the round pegs in the +round+ square holes...
     describe('unclosed property drawer is treated as text', () => {
       testParse('basic',
         "* Heading\n:PROPERTIES:",
-        [section(1, 'Heading', [{
-          type: 'paragraph',
-          attributes: {},
-          children: [text(':PROPERTIES:', { position: pos([2, 1], [2, 13]) })],
-          position: pos([2, 1], [2, 13]),
-        }])]);
+        [section(1, 'Heading', [paragraph(
+          [text(':PROPERTIES:', { position: pos([2, 1], [2, 13]) })],
+          { position: pos([2, 1], [2, 13]) })])]);
 
       testParse('casing is preserved',
         "* Heading\n:PRopErTIES:",
-        [section(1, 'Heading', [{
-          type: 'paragraph',
-          attributes: {},
-          children: [text(':PRopErTIES:', { position: pos([2, 1], [2, 13]) })],
-          position: pos([2, 1], [2, 13]),
-        }])]);
+        [section(1, 'Heading', [paragraph(
+          [text(':PRopErTIES:', { position: pos([2, 1], [2, 13]) })],
+          { position: pos([2, 1], [2, 13]) })])]);
 
       testParse('with extra text',
         "* Heading\n:PROPERTIES:\nmore text",
-        [section(1, 'Heading', [{
-          type: 'paragraph',
-          attributes: {},
-          children: [
+        [section(1, 'Heading', [paragraph(
+          [
             text(':PROPERTIES:', { position: pos([2, 1], [2, 13]) }),
             text(' ', { position: pos([2, 13], [3, 1]) }),
             text('more text', { position: pos([3, 1], [3, 10]) })],
-          position: pos([2, 1], [3, 10]),
-        }])]);
+          { position: pos([2, 1], [3, 10]) })])]);
     });
   });
 
