2127efe8360fc7fa84ed48fc864bd18e9d8c10b5
Ben Moon <software@guiltydolphin.com>
2021-07-03 16:02:18 +0100

    Add support for disabling lexer debug colouring

--- packages/orga/src/tokenize/__tests__/debug.ts
+++ packages/orga/src/tokenize/__tests__/debug.ts
@@ -3,8 +3,20 @@ import { tokenize } from '../index'
 import { inspect } from 'util'
 import chalk from 'chalk'
 
-export default (text: string): void => {
+export default (text: string, useColor = true): void => {
   const { substring } = read(text)
+
+  const colored = (color: 'red' | 'gray') => {
+    if (useColor) {
+      return color === 'red' ? chalk.red : chalk.gray;
+    } else {
+      return (text: string) => text;
+    }
+  }
+
+  const red = colored('red');
+  const gray = colored('gray');
+
   const tokens = tokenize(text).all()
   const data = tokens.map(token => ({
     ...token,
@@ -12,12 +24,12 @@ export default (text: string): void => {
   }))
 
   const lines = [
-    chalk.red('** DEBUG **'),
-    chalk.red('> text:'),
-    chalk.gray(text),
-    chalk.red('> tokens:'),
+    red('** DEBUG **'),
+    red('> text:'),
+    gray(text),
+    red('> tokens:'),
   ].join('\n')
 
-  console.log(lines, inspect(data, false, null, true))
+  console.log(lines, inspect(data, false, null, useColor))
   // console.log(inspect(data, false, null, true))
 }
