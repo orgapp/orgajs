209cc08ab2283b93984cc8c971c538d7c3c4fe9e
Ben Moon <software@guiltydolphin.com>
2021-07-05 15:28:52 +0100

    Refactor block token tests

--- packages/orga/src/tokenize/__tests__/block.spec.ts
+++ packages/orga/src/tokenize/__tests__/block.spec.ts
@@ -1,394 +1,94 @@
-import tok from "./tok"
+import {
+  testLexer,
+  testLexerMulti,
+  tokBlockBegin,
+  tokBlockEnd,
+  tokNewline,
+  tokText,
+  tokTextBold,
+} from './util';
+
+import { Token } from '../../types';
 
 describe("tokenize block", () => {
-  it("knows block begins", () => {
-    expect(tok("#+BEGIN_SRC swift")).toMatchInlineSnapshot(`
-      Array [
-        Object {
-          "_text": "#+BEGIN_SRC swift",
-          "name": "SRC",
-          "params": Array [
-            "swift",
-          ],
-          "type": "block.begin",
-        },
-      ]
-    `)
+  const testBlockBegin = (text: string, name: string, params: string[]): Parameters<typeof testLexerMulti>[1][number] => {
+    return [text, [tokBlockBegin(name, { _text: text, params })]];
+  };
 
-    expect(tok("#+begin_src swift")).toMatchInlineSnapshot(`
-      Array [
-        Object {
-          "_text": "#+begin_src swift",
-          "name": "src",
-          "params": Array [
-            "swift",
-          ],
-          "type": "block.begin",
-        },
-      ]
-    `)
-    expect(tok("#+begin_example")).toMatchInlineSnapshot(`
-      Array [
-        Object {
-          "_text": "#+begin_example",
-          "name": "example",
-          "params": Array [],
-          "type": "block.begin",
-        },
-      ]
-    `)
-    expect(tok("#+begin_ex😀mple")).toMatchInlineSnapshot(`
-      Array [
-        Object {
-          "_text": "#+begin_ex😀mple",
-          "name": "ex😀mple",
-          "params": Array [],
-          "type": "block.begin",
-        },
-      ]
-    `)
-    expect(tok("#+begin_src swift :tangle code.swift")).toMatchInlineSnapshot(`
-      Array [
-        Object {
-          "_text": "#+begin_src swift :tangle code.swift",
-          "name": "src",
-          "params": Array [
-            "swift",
-            ":tangle",
-            "code.swift",
-          ],
-          "type": "block.begin",
-        },
-      ]
-    `)
-  })
+  testLexerMulti("knows block begins", [
+    testBlockBegin("#+BEGIN_SRC swift", "SRC", ["swift"]),
+    testBlockBegin("#+begin_src swift", "src", ["swift"]),
+    testBlockBegin("#+begin_example", "example", []),
+    testBlockBegin("#+begin_ex😀mple", "ex😀mple", []),
+    testBlockBegin("#+begin_src swift :tangle code.swift", "src", ["swift", ":tangle", "code.swift"]),
+  ]);
 
-  it("knows these are not block begins", () => {
-    expect(tok("#+begi😀n_src swift")).toMatchInlineSnapshot(`
-      Array [
-        Object {
-          "_text": "#+begi😀n_src swift",
-          "type": "text.plain",
-          "value": "#+begi😀n_src swift",
-        },
-      ]
-    `)
-  })
+  testLexer("knows these are not block begins",
+    "#+begi😀n_src swift",
+    [tokText("#+begi😀n_src swift")]);
 
-  it("knows block ends", () => {
-    expect(tok("#+END_SRC")).toMatchInlineSnapshot(`
-      Array [
-        Object {
-          "_text": "#+END_SRC",
-          "name": "SRC",
-          "type": "block.end",
-        },
-      ]
-    `)
-    expect(tok("  #+END_SRC")).toMatchInlineSnapshot(`
-      Array [
-        Object {
-          "_text": "#+END_SRC",
-          "name": "SRC",
-          "type": "block.end",
-        },
-      ]
-    `)
-    expect(tok("#+end_src")).toMatchInlineSnapshot(`
-      Array [
-        Object {
-          "_text": "#+end_src",
-          "name": "src",
-          "type": "block.end",
-        },
-      ]
-    `)
-    expect(tok("#+end_SRC")).toMatchInlineSnapshot(`
-      Array [
-        Object {
-          "_text": "#+end_SRC",
-          "name": "SRC",
-          "type": "block.end",
-        },
-      ]
-    `)
-    expect(tok("#+end_S😀RC")).toMatchInlineSnapshot(`
-      Array [
-        Object {
-          "_text": "#+end_S😀RC",
-          "name": "S😀RC",
-          "type": "block.end",
-        },
-      ]
-    `)
-    expect(tok("#+end_SRC ")).toMatchInlineSnapshot(`
-      Array [
-        Object {
-          "_text": "#+end_SRC ",
-          "name": "SRC",
-          "type": "block.end",
-        },
-      ]
-    `)
-  })
+  const testBlockEnd = (text: string, name: string): Parameters<typeof testLexerMulti>[1][number] => {
+    return [text, [tokBlockEnd(name, { _text: text })]];
+  };
 
-  it("knows these are not block ends", () => {
-    expect(tok("#+end_src param")).toMatchInlineSnapshot(`
-      Array [
-        Object {
-          "_text": "#+end_src param",
-          "type": "text.plain",
-          "value": "#+end_src param",
-        },
-      ]
-    `)
-  })
+  testLexerMulti("knows block ends", [
+    testBlockEnd("#+END_SRC", "SRC"),
+    // not using testBlockEnd for this because the leading spaces get stripped (2021-07-05)
+    ["  #+END_SRC", [tokBlockEnd("SRC", { _text: "#+END_SRC" })]],
+    testBlockEnd("#+end_src", "src"),
+    testBlockEnd("#+end_SRC", "SRC"),
+    testBlockEnd("#+end_S😀RC", "S😀RC"),
+    testBlockEnd("#+end_SRC ", "SRC"),
+  ]);
+
+  testLexer("knows these are not block ends",
+    "#+end_src param",
+    [tokText("#+end_src param")]);
 
   describe("verse blocks", () => {
-    it('inner block tokenized as text', () => {
-      expect(tok(`#+BEGIN_VERSE
-#+BEGIN_SRC ts
+    const testVerseBlock = (testName: string, innerText: string, innerBody: Token[]) => {
+      testLexer(testName, `#+BEGIN_VERSE\n${innerText}\n#+END_VERSE`, [
+        tokBlockBegin("VERSE"),
+        tokNewline(),
+        ...innerBody,
+        tokNewline(),
+        tokBlockEnd("VERSE"),
+      ]);
+    };
+
+    testVerseBlock("inner block tokenized as text", `#+BEGIN_SRC ts
 function () {}
-#+END_SRC
-#+END_VERSE`)).toMatchInlineSnapshot(`
-      Array [
-        Object {
-          "_text": "#+BEGIN_VERSE",
-          "name": "VERSE",
-          "params": Array [],
-          "type": "block.begin",
-        },
-        Object {
-          "_text": "
-      ",
-          "type": "newline",
-        },
-        Object {
-          "_text": "#+BEGIN_SRC ts",
-          "type": "text.plain",
-          "value": "#+BEGIN_SRC ts",
-        },
-        Object {
-          "_text": "
-      ",
-          "type": "newline",
-        },
-        Object {
-          "_text": "function () {}",
-          "type": "text.plain",
-          "value": "function () {}",
-        },
-        Object {
-          "_text": "
-      ",
-          "type": "newline",
-        },
-        Object {
-          "_text": "#+END_SRC",
-          "type": "text.plain",
-          "value": "#+END_SRC",
-        },
-        Object {
-          "_text": "
-      ",
-          "type": "newline",
-        },
-        Object {
-          "_text": "#+END_VERSE",
-          "name": "VERSE",
-          "type": "block.end",
-        },
-      ]
-    `)
-    });
+#+END_SRC`, [
+      tokText("#+BEGIN_SRC ts"),
+      tokNewline(),
+      tokText("function () {}"),
+      tokNewline(),
+      tokText("#+END_SRC"),
+    ]);
 
-    it('inner block with markup', () => {
-      expect(tok(`#+BEGIN_VERSE
-#+BEGIN_EXAMPLE *text*
+    testVerseBlock("inner block with markup", `#+BEGIN_EXAMPLE *text*
 more text
-#+END_EXAMPLE
-#+END_VERSE`)).toMatchInlineSnapshot(`
-      Array [
-        Object {
-          "_text": "#+BEGIN_VERSE",
-          "name": "VERSE",
-          "params": Array [],
-          "type": "block.begin",
-        },
-        Object {
-          "_text": "
-      ",
-          "type": "newline",
-        },
-        Object {
-          "_text": "#+BEGIN_EXAMPLE ",
-          "type": "text.plain",
-          "value": "#+BEGIN_EXAMPLE ",
-        },
-        Object {
-          "_text": "*text*",
-          "type": "text.bold",
-          "value": "text",
-        },
-        Object {
-          "_text": "
-      ",
-          "type": "newline",
-        },
-        Object {
-          "_text": "more text",
-          "type": "text.plain",
-          "value": "more text",
-        },
-        Object {
-          "_text": "
-      ",
-          "type": "newline",
-        },
-        Object {
-          "_text": "#+END_EXAMPLE",
-          "type": "text.plain",
-          "value": "#+END_EXAMPLE",
-        },
-        Object {
-          "_text": "
-      ",
-          "type": "newline",
-        },
-        Object {
-          "_text": "#+END_VERSE",
-          "name": "VERSE",
-          "type": "block.end",
-        },
-      ]
-    `)
-    });
+#+END_EXAMPLE`, [
+      tokText("#+BEGIN_EXAMPLE "), tokTextBold("text"),
+      tokNewline(),
+      tokText("more text"),
+      tokNewline(),
+      tokText("#+END_EXAMPLE"),
+    ]);
 
-    it('heading with markup', () => {
-      expect(tok(`#+BEGIN_VERSE
-* Heading *with markup*
-#+END_VERSE`)).toMatchInlineSnapshot(`
-      Array [
-        Object {
-          "_text": "#+BEGIN_VERSE",
-          "name": "VERSE",
-          "params": Array [],
-          "type": "block.begin",
-        },
-        Object {
-          "_text": "
-      ",
-          "type": "newline",
-        },
-        Object {
-          "_text": "* Heading ",
-          "type": "text.plain",
-          "value": "* Heading ",
-        },
-        Object {
-          "_text": "*with markup*",
-          "type": "text.bold",
-          "value": "with markup",
-        },
-        Object {
-          "_text": "
-      ",
-          "type": "newline",
-        },
-        Object {
-          "_text": "#+END_VERSE",
-          "name": "VERSE",
-          "type": "block.end",
-        },
-      ]
-    `)
-    });
+    testVerseBlock("heading with markup", "* Heading *with markup*", [
+      tokText("* Heading "), tokTextBold("with markup"),
+    ]);
 
-    it('lists not tokenized', () => {
-      expect(tok(`#+BEGIN_VERSE
-- this is not lexed
+    testVerseBlock("lists not tokenized", `- this is not lexed
 
-1. nor is this
-#+END_VERSE`)).toMatchInlineSnapshot(`
-      Array [
-        Object {
-          "_text": "#+BEGIN_VERSE",
-          "name": "VERSE",
-          "params": Array [],
-          "type": "block.begin",
-        },
-        Object {
-          "_text": "
-      ",
-          "type": "newline",
-        },
-        Object {
-          "_text": "- this is not lexed",
-          "type": "text.plain",
-          "value": "- this is not lexed",
-        },
-        Object {
-          "_text": "
-      ",
-          "type": "newline",
-        },
-        Object {
-          "_text": "
-      ",
-          "type": "newline",
-        },
-        Object {
-          "_text": "1. nor is this",
-          "type": "text.plain",
-          "value": "1. nor is this",
-        },
-        Object {
-          "_text": "
-      ",
-          "type": "newline",
-        },
-        Object {
-          "_text": "#+END_VERSE",
-          "name": "VERSE",
-          "type": "block.end",
-        },
-      ]
-    `)
-    });
+1. nor is this`, [
+      tokText("- this is not lexed"),
+      tokNewline(),
+      tokNewline(),
+      tokText("1. nor is this"),
+    ]);
 
-    it('comments not tokenized', () => {
-      expect(tok(`#+BEGIN_VERSE
-# comment
-#+END_VERSE`)).toMatchInlineSnapshot(`
-      Array [
-        Object {
-          "_text": "#+BEGIN_VERSE",
-          "name": "VERSE",
-          "params": Array [],
-          "type": "block.begin",
-        },
-        Object {
-          "_text": "
-      ",
-          "type": "newline",
-        },
-        Object {
-          "_text": "# comment",
-          "type": "text.plain",
-          "value": "# comment",
-        },
-        Object {
-          "_text": "
-      ",
-          "type": "newline",
-        },
-        Object {
-          "_text": "#+END_VERSE",
-          "name": "VERSE",
-          "type": "block.end",
-        },
-      ]
-    `)
-    });
+    testVerseBlock("comments not tokenized", "# comment", [tokText("# comment")]);
   });
 })
---
+++ packages/orga/src/tokenize/__tests__/util.ts
@@ -0,0 +1,56 @@
+import {
+  BlockBegin,
+  BlockEnd,
+  Newline,
+  StyledText,
+  Token,
+} from '../../types';
+import tok from './tok';
+
+export function testLexer(testName: string, input: string, expected: Token[]) {
+  it(testName, () => {
+    expect(tok(input)).toMatchObject(expected);
+  });
+}
+
+export function testLexerMulti(testName: string, tests: [input: string, expected: Token[]][]) {
+  it(testName, () => {
+    for (const [input, expected] of tests) {
+      expect(tok(input)).toMatchObject(expected);
+    }
+  });
+}
+
+type Extra<Tok extends Token, Keys extends keyof (Tok & { _text: string }) = 'type'> = Partial<Omit<Tok | { _text: string }, Keys | 'type'>>;
+
+export const tokBlockBegin = (name: string, extra: Extra<BlockBegin, 'name'> = {}): BlockBegin => ({
+  type: 'block.begin',
+  name,
+  params: [],
+  ...{ _text: `#+BEGIN_${name}` },
+  ...extra,
+});
+
+export const tokBlockEnd = (name: string, extra: Extra<BlockEnd, 'name'> = {}): BlockEnd => ({
+  type: 'block.end',
+  name,
+  ...{ _text: `#+END_${name}` },
+  ...extra,
+});
+
+export const tokNewline = (extra: Extra<Newline, '_text'> = {}): Newline => ({
+  type: 'newline',
+  ...{ _text: '\n' },
+  ...extra,
+});
+
+export const tokStyledText = <TextTy extends StyledText['type']>(type: TextTy, marker: string) => (value: string, extra: Extra<StyledText, 'value'> = {}): StyledText & { type: TextTy } => ({
+  type,
+  value,
+  ...{ _text: `${marker}${value}${marker}` },
+  ...extra,
+});
+
+export const tokText = tokStyledText('text.plain', '');
+
+export const tokTextBold = tokStyledText('text.bold', '*');
