3d0e816312dcfc0b385920be0cc2461d85cee5e9
Ben Moon <software@guiltydolphin.com>
2021-07-12 11:08:51 +0100

    Add todo

--- packages/orga/src/tokenize/__tests__/timestamp.spec.ts
+++ packages/orga/src/tokenize/__tests__/timestamp.spec.ts
@@ -6,6 +6,9 @@ const p = (text: string) => {
 
 describe("timestamps", () => {
   it("can parse timestamps", () => {
+    // TODO: fix this - this isn't correct because it should just be
+    // 2021-04-24 in the user's LOCAL TIME - we shouldn't be making
+    // assumptions yet (2021-07-12)
     expect(p("[2021-04-24 Sat]")).toMatchInlineSnapshot(`
       Object {
         "date": 2021-04-23T12:00:00.000Z,
