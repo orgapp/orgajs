ba0f65328868531199138672a54936d29c7a8845
Xiaoxing Hu <xiaoxing@huxx.org>
2021-09-16 16:18:49 +1200

    new package: gatsby-plugin-orga-theme-ui

---
+++ packages/gatsby-plugin-orga-theme-ui/gatsby-browser.js
@@ -0,0 +1,7 @@
+/* eslint no-unused-vars : "off" */
+import * as React from 'react'
+import { WrapRootElement } from './src/provider'
+
+export const wrapRootElement = ({ element }) => (
+  <WrapRootElement element={element} />
+)
---
+++ packages/gatsby-plugin-orga-theme-ui/src/provider.tsx
@@ -0,0 +1,31 @@
+import { ThemeProvider, merge } from '@orgajs/theme-ui'
+import React from 'react'
+
+import localTheme from './index'
+import components from './components'
+import useThemeUiConfig from './hooks/options'
+
+const Root = ({ children }) => {
+  const themeUiConfig = useThemeUiConfig()
+  const { preset, prismPreset } = themeUiConfig
+
+  const theme = preset.default || preset
+
+  const themeWithPrism = merge(theme, {
+    styles: {
+      pre: prismPreset,
+    },
+  })
+
+  const fullTheme = merge(themeWithPrism, localTheme)
+
+  return (
+    <ThemeProvider theme={fullTheme} components={components}>
+      {children}
+    </ThemeProvider>
+  )
+}
+
+export const WrapRootElement = ({ element }) => {
+  return <Root>{element}</Root>
+}
