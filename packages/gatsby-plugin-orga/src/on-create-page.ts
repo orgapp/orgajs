3965fe5cd13f05f19a803c97676a188de7f0207e
Xiaoxing Hu <xiaoxing@huxx.org>
2021-09-20 20:50:00 +1200

    gatsby-plugin-orga: rename properties to metadata

--- packages/gatsby-plugin-orga/src/on-create-page.ts
+++ packages/gatsby-plugin-orga/src/on-create-page.ts
@@ -1,24 +1,20 @@
 import * as fs from 'fs-extra'
 import { GatsbyNode } from 'gatsby'
-import metadata from '@orgajs/metadata'
+import parse from '@orgajs/metadata'
 
-const onCreatePage: GatsbyNode['onCreatePage'] = async (
-  { page, cache, actions },
-  options
-) => {
+const onCreatePage: GatsbyNode['onCreatePage'] = async ({ page, actions }) => {
   if (page.context.properties) return
 
   const { deletePage, createPage } = actions
 
   const content = await fs.readFile(page.component, `utf8`)
-  const properties = metadata(content)
-  console.log(properties)
+  const metadata = parse(content)
   deletePage(page)
   createPage({
     ...page,
     context: {
       ...page.context,
-      properties,
+      metadata,
       file: page.component,
     },
   })
