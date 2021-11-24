7faae36fcbe5a93e75988a37e92a175d508aed7c
Xiaoxing Hu <xiaoxing@huxx.org>
2021-11-24 13:28:11 +1300

    nextjs integration: add option to set heading offset

--- packages/oast-to-hast/src/handlers/headline.ts
+++ packages/oast-to-hast/src/handlers/headline.ts
@@ -3,5 +3,7 @@ import { Context } from '../'
 
 export default (node: Headline, context: Context) => {
   const { h, all } = context
-  return h(`h${node.level}`)(...all(node.children, context))
+  return h(`h${node.level + context.headingOffset}`)(
+    ...all(node.children, context)
+  )
 }
