e5b40c7e1ea5885dc4b0b69c58f5a4ec5a057ef2
Xiaoxing Hu <hi@xiaoxing.dev>
2023-05-01 20:35:59 +1200

    convert @orgajs/loader to esm

---
+++ packages/loader/index.cjs
@@ -0,0 +1,18 @@
+'use strict'
+// stolen code from @jdx-js/loader
+
+/**
+ * Webpack loader
+ *
+ * @todo once webpack supports ESM loaders, remove this wrapper.
+ *
+ * @this {LoaderContext}
+ * @param {string} code
+ */
+module.exports = function (code) {
+  const callback = this.async()
+  // Note that `import()` caches, so this should be fast enough.
+  import('./dist/index.js').then((module) =>
+    module.default.call(this, code, callback)
+  )
+}
