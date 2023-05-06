75649729753ebe33e1628ec7824f737e5edcef86
Xiaoxing Hu <hi@xiaoxing.dev>
2023-05-06 17:54:22 +1200

    to ESM, metadata

--- packages/metadata/src/index.test.ts
+++ packages/metadata/src/index.test.ts
@@ -1,75 +1,62 @@
+import { describe, it } from 'node:test'
+import assert from 'node:assert'
 import parse from '.'
 
 describe('metadata parser', () => {
   it('works', () => {
-    expect(parse('#+title: hello world')).toMatchInlineSnapshot(`
-      Object {
-        "title": "hello world",
-      }
-    `)
+    assert.deepEqual(parse('#+title: hello world'), {
+      title: 'hello world',
+    })
   })
 
   it('can handle multiple entries', () => {
-    expect(
+    assert.deepEqual(
       parse(`
 #+include: file1.org
 #+include: ../file2.org
-`)
-    ).toMatchInlineSnapshot(`
-      Object {
-        "include": Array [
-          "file1.org",
-          "../file2.org",
-        ],
+`),
+      {
+        include: ['file1.org', '../file2.org'],
       }
-    `)
+    )
   })
 
   it('can handle spaces at the front', () => {
-    expect(
+    assert.deepEqual(
       parse(`
-  #+title: orga
-    #+keywords: parser ast
-`)
-    ).toMatchInlineSnapshot(`
-      Object {
-        "keywords": "parser ast",
-        "title": "orga",
+#+title: orga
+#+keywords: parser ast
+`),
+      {
+        title: 'orga',
+        keywords: 'parser ast',
       }
-    `)
+    )
   })
 
   it('transform keys to lowercase', () => {
-    expect(
+    assert.deepEqual(
       parse(`
 #+TODO: TODO NEXT | DONE
 #+todo: DRAFT | PUBLISHED
-`)
-    ).toMatchInlineSnapshot(`
-      Object {
-        "todo": Array [
-          "TODO NEXT | DONE",
-          "DRAFT | PUBLISHED",
-        ],
+`),
+      {
+        todo: ['TODO NEXT | DONE', 'DRAFT | PUBLISHED'],
       }
-    `)
+    )
   })
 
   it('strips quotes if necessary', () => {
-    expect(
+    assert.deepEqual(
       parse(`
 #+include: "./file1.org"
 #+include: './file2.org'
 #+desc: it's good
-`)
-    ).toMatchInlineSnapshot(`
-      Object {
-        "desc": "it's good",
-        "include": Array [
-          "./file1.org",
-          "./file2.org",
-        ],
+`),
+      {
+        include: ['./file1.org', './file2.org'],
+        desc: "it's good",
       }
-    `)
+    )
   })
 })
