8caa3ff1797fd1fa76911a80e8e2f92098bdccf0
Xiaoxing Hu <hi@xiaoxing.dev>
2023-05-08 11:17:53 +1200

    remove custom code in rehype-estree
    use hast-util-to-estree instead

--- packages/rehype-estree/src/index.ts
+++ packages/rehype-estree/src/index.ts
@@ -1,38 +1,10 @@
 import { Transformer } from 'unified'
-import toEstree from './hast-to-estree'
-import { DEFAULT_OPTIONS, Options } from './options'
-import processEstree from './process-estree'
-import { removeQuotes } from './utils'
+import { toEstree, Options } from 'hast-util-to-estree'
 
 export type { Options }
 
-const getLastValue = (key: string, data: any) => {
-  const value = data[key]
-  if (!value) return
-  if (Array.isArray(value)) {
-    return value[value.length - 1]
-  }
-  return value
-}
-
 function rehype2estree(options: Partial<Options> = {}): Transformer {
-  return transformer
-
-  function transformer(node, file) {
-    const _options = { ...DEFAULT_OPTIONS, ...options }
-    const layout = getLastValue('layout', node.data)
-    if (typeof layout === 'string') {
-      const _layout = removeQuotes(layout)
-      if (['', 'nil', 'undefined', 'null'].includes(_layout)) {
-        _options.defaultLayout = undefined
-      } else {
-        _options.defaultLayout = _layout
-      }
-    }
-
-    const estree = toEstree(node, _options)
-    return processEstree(estree, _options)
-  }
+  return (tree) => toEstree(tree, options)
 }
 
 export default rehype2estree
