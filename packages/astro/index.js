f7e7f8d1bf53f36ebfa0969aeed1ed06ab7b0391
Xiaoxing Hu <hi@xiaoxing.dev>
2023-05-19 22:06:48 +1200

    add @orgajs/astro plugin

---
+++ packages/astro/index.js
@@ -0,0 +1,46 @@
+/**
+ * @typedef {import('astro').HookParameters<'astro:config:setup'> & {
+ *   addPageExtension: (ext: string) => void;
+ *   addContentEntryType: (contentEntryType: import('astro').ContentEntryType) => void;
+ * }} SetupHookParams
+ */
+import orga from '@orgajs/rollup'
+import parseMetadata from '@orgajs/metadata'
+
+/**
+ * @returns {import('astro').AstroIntegration}
+ */
+export default function withOrga() {
+  return {
+    name: '@orgajs/astro',
+    hooks: {
+      'astro:config:setup': async (params) => {
+        const { addPageExtension, addContentEntryType, updateConfig } =
+          /** @type {SetupHookParams} */ params
+        addPageExtension('.org')
+
+        addContentEntryType({
+          extensions: ['.org'],
+          async getEntryInfo({ contents }) {
+            const data = parseMetadata(contents)
+            return {
+              data,
+              contents,
+            }
+          },
+        })
+
+        updateConfig({
+          vite: {
+            plugins: [
+              {
+                enforce: 'pre',
+                ...orga(),
+              },
+            ],
+          },
+        })
+      },
+    },
+  }
+}
