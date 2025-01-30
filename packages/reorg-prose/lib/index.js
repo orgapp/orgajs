6cfb4a5cfad62f5dbbea27869fca265e0d4ff68f
Xiaoxing Hu <hi@xiaoxing.dev>
2025-01-30 18:35:33 +1300

    remove comments

--- packages/reorg-prose/lib/index.js
+++ packages/reorg-prose/lib/index.js
@@ -2,33 +2,7 @@
  * @import {Document as OastRoot} from 'orga'
  * @import {Node as ProseNode} from 'prosemirror-model'
  * @import {Options} from 'oast-to-prose'
- * @import {Processor} from 'unified'
  * @import {VFile} from 'vfile'
- */
-
-/**
- * @callback TransformBridge
- *   Bridge-mode.
- *
- *   Runs the destination with the new hast tree.
- *   Discards result.
- * @param {OastRoot} tree
- *   Tree.
- * @param {VFile} file
- *   File.
- * @returns {Promise<undefined>}
- *   Nothing.
- *
- * @callback TransformMutate
- *  Mutate-mode.
- *
- *  Further transformers run on the hast tree.
- * @param {OastRoot} tree
- *   Tree.
- * @param {VFile} file
- *   File.
- * @returns {ProseNode}
- *   Tree (prose node).
  */
 
 import { toProse } from 'oast-to-prose'
@@ -51,22 +25,6 @@ export default function reorgProse(options) {
    *   Prose Node.
    */
   return function (tree, file) {
-    // Cast because root in -> root out.
-    // To do: in the future, disallow ` || options` fallback.
-    // With `unified-engine`, `destination` can be `undefined` but
-    // `options` will be the file set.
-    // We should not pass that as `options`.
     return /** @type {ProseNode} */ (toProse(tree, file, options))
-
-    // this.compiler = compiler
-
-    // /**
-    //  * @param {Input} tree
-    //  * @param {import('vfile').VFile} file
-    //  * @returns {Output}
-    //  */
-    // function compiler(tree, file) {
-    //   return toProse(tree, file, options)
-    // }
   }
 }
