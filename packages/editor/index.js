fdd455a8fe67ca44155b0ad4faa6b7987e4ab462
Xiaoxing Hu <hi@xiaoxing.dev>
2025-02-14 18:51:34 +1300

    fix build

---
+++ packages/editor/index.js
@@ -0,0 +1,6 @@
+/**
+ * @typedef {import('./lib/editor.js').Config} EditorConfig
+ */
+
+export { makeEditor } from './lib/editor.js'
+export { tags } from '@orgajs/cm-lang'
--- packages/editor/lib/index.js
+++
@@ -1,6 +0,0 @@
-/**
- * @typedef {import('./editor.js').Config} EditorConfig
- */
-
-export { makeEditor } from './editor.js'
-export { tags } from '@orgajs/cm-lang'
