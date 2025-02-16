20391c76ee5ef690863431a6831e64e5ae60722e
Xiaoxing Hu <hi@xiaoxing.dev>
2025-02-16 19:48:56 +1300

    fix reorg-parse

--- packages/reorg-parse/index.js
+++ packages/reorg-parse/index.js
@@ -1,1 +1,26 @@
-export { default } from './lib/index.js'
+/**
+ * @import {Document, ParseOptions} from 'orga'
+ * @import {Processor} from 'unified'
+ */
+
+/**
+ * @typedef {ParseOptions} Options
+ */
+
+import { parse } from 'orga'
+
+/**
+ * Aadd support for parsing from org-mode.
+ *
+ * @this {Processor<Document>}
+ *   Processor instance.
+ * @param {Partial<ParseOptions> | undefined} [options]
+ *   Configuration (optional).
+ * @returns {undefined}
+ *   Nothing.
+ */
+export default function reorgParse(options) {
+	this.parser = function (doc) {
+		return parse(doc, options)
+	}
+}
