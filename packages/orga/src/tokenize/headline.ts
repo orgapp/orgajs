dd8da5630dbab1a6c9eda663933d78991d81e2ea
Xiaoxing Hu <hi@xiaoxing.dev>
2025-04-28 12:29:54 +1200

    fix headline pattern issue

--- packages/orga/src/tokenize/headline.ts
+++ packages/orga/src/tokenize/headline.ts
@@ -6,84 +6,84 @@ import { tokenize } from './inline/index.js'
 type GetTodoKeywordSets = () => TodoKeywordSet[]
 
 export default (getTodoKeywordSets: GetTodoKeywordSets) =>
-  (reader: Reader): Token[] | void => {
-    const { isStartOfLine, match, now, eol, eat, jump, substring, endOfLine } =
-      reader
+	(reader: Reader): Token[] | void => {
+		const { isStartOfLine, match, now, eol, eat, jump, substring, endOfLine } =
+			reader
 
-    const todoKeywordSets = getTodoKeywordSets()
+		const todoKeywordSets = getTodoKeywordSets()
 
-    if (!isStartOfLine() || !match(/^\*+\s+/my)) return
+		if (!isStartOfLine() || !match(/^\*+[ \t]+/my)) return
 
-    // TODO: cache this, for performance sake
-    const todos = todoKeywordSets.flatMap((s) => s.keywords)
+		// TODO: cache this, for performance sake
+		const todos = todoKeywordSets.flatMap((s) => s.keywords)
 
-    const isActionable = (keyword: string): boolean => {
-      return !!todoKeywordSets.find((s) => s.actionables.includes(keyword))
-    }
+		const isActionable = (keyword: string): boolean => {
+			return !!todoKeywordSets.find((s) => s.actionables.includes(keyword))
+		}
 
-    let buffer: Token[] = []
+		let buffer: Token[] = []
 
-    const stars = eat(/^\*+(?=\s)/)
-    if (!stars) throw Error('not gonna happen')
-    buffer.push({
-      type: 'stars',
-      level: stars.value.length,
-      position: {
-        start: stars.position.start,
-        end: eat('whitespaces').position.end,
-      },
-    })
-    const keyword = eat(RegExp(`${todos.map(escape).join('|')}(?=\\s)`, 'y'))
-    if (keyword) {
-      buffer.push({
-        type: 'todo',
-        keyword: keyword.value,
-        actionable: isActionable(keyword.value),
-        position: {
-          start: keyword.position.start,
-          end: eat('whitespaces').position.end,
-        },
-      })
-    }
-    const priority = eat(/^\[#(A|B|C)\](?=\s)/y)
-    if (priority) {
-      buffer.push({
-        type: 'priority',
-        ...priority,
-        position: {
-          start: priority.position.start,
-          end: eat('whitespaces').position.end,
-        },
-      })
-    }
+		const stars = eat(/^\*+(?=\s)/)
+		if (!stars) throw Error('not gonna happen')
+		buffer.push({
+			type: 'stars',
+			level: stars.value.length,
+			position: {
+				start: stars.position.start,
+				end: eat('whitespaces').position.end
+			}
+		})
+		const keyword = eat(RegExp(`${todos.map(escape).join('|')}(?=\\s)`, 'y'))
+		if (keyword) {
+			buffer.push({
+				type: 'todo',
+				keyword: keyword.value,
+				actionable: isActionable(keyword.value),
+				position: {
+					start: keyword.position.start,
+					end: eat('whitespaces').position.end
+				}
+			})
+		}
+		const priority = eat(/^\[#(A|B|C)\](?=\s)/y)
+		if (priority) {
+			buffer.push({
+				type: 'priority',
+				...priority,
+				position: {
+					start: priority.position.start,
+					end: eat('whitespaces').position.end
+				}
+			})
+		}
 
-    const tags = match(/[ \t]+(:(?:[\w@_#%-]+:)+)[ \t]*$/m, {
-      end: endOfLine(),
-    })
-    let contentEnd = eol(now().line)
-    if (tags) {
-      contentEnd = tags.position.start
-    }
+		const tags = match(/[ \t]+(:(?:[\w@_#%-]+:)+)[ \t]*$/m, {
+			end: endOfLine()
+		})
+		let contentEnd = eol(now().line)
+		if (tags) {
+			contentEnd = tags.position.start
+		}
 
-    const r = reader.read({ end: contentEnd })
-    const tokens = tokenize(r)
-    jump(r.now())
+		const r = reader.read({ end: contentEnd })
+		const tokens = tokenize(r)
+		jump(r.now())
 
-    buffer = buffer.concat(tokens)
+		buffer = buffer.concat(tokens)
 
-    if (tags) {
-      eat('whitespaces')
-      const tagsPosition = { start: now(), end: tags.position.end }
-      const s = substring(tagsPosition.start, tagsPosition.end)
-      buffer.push({
-        type: 'tags',
-        tags: s
-          .split(':')
-          .map((t) => t.trim())
-          .filter(Boolean),
-        position: { start: now(), end: tags.position.end },
-      })
-      jump(tags.position.end)
-    }
-    return buffer
-  }
+		if (tags) {
+			eat('whitespaces')
+			const tagsPosition = { start: now(), end: tags.position.end }
+			const s = substring(tagsPosition.start, tagsPosition.end)
+			buffer.push({
+				type: 'tags',
+				tags: s
+					.split(':')
+					.map((t) => t.trim())
+					.filter(Boolean),
+				position: { start: now(), end: tags.position.end }
+			})
+			jump(tags.position.end)
+		}
+		return buffer
+	}
